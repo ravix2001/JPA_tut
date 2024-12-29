378cb6141991723cdf941c157f082183b4423d5d
Ravi <ravipandit2001.11.02@gmail.com>
2024-12-29 08:11:53 +0545

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,34 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Get data from database
+        /*EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManager em = emf.createEntityManager();
+
+        Alien alien = em.find(Alien.class, 1);
+
+        System.out.println(alien);*/
+
+        //Persist(save) data into database
+        Alien a = new Alien();
+        a.setId(5);
+        a.setName("Anish");
+        a.setTech("Engineering");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(a);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+        System.out.println(a);
     }
 }
